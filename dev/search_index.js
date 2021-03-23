99e4ccccb9de3f203ed13182bc86721a28b108b2
Documenter.jl <documenter@juliadocs.github.io>
2021-03-23 00:03:58 +0000

    build based on 078cdfa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BigO","category":"page"},{"location":"#BigO","page":"Home","title":"BigO","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BigO.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BigO]","category":"page"},{"location":"#BigO.RunReport","page":"Home","title":"BigO.RunReport","text":"RunReport\n\n\n\n\n\n","category":"type"}]
+}
